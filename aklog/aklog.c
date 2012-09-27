941466b60c0fb512eb46ddb055f2b505bdd31ccb
Jianxun Zhang <jianxun.zhang@intel.com>
2012-09-27 10:44:44 -0700

    aklog: add log rotation feature.
    Support rotating logs among a set of log files whose's size and
    number are specified in the values of new '-s' and '-n' arguments.
    
    This is for the readiness of the offline kenrel log.
    
    For: AXIA-765
    Change-Id: I1c5b5a1fdd7d41ed5b0c1a73f15c15c84d260c79
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- aklog/aklog.c
+++ aklog/aklog.c
@@ -23,17 +23,32 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-#define LOG_BUF_SZ (1024 * 128)
+#define min(x,y) ((x) < (y) ? (x) : (y))
+#define KB 1024
+#define LOG_BUF_SZ (KB * 128)
 #define ERR(fmt...) fprintf(stderr, fmt)
+#define UNLIMITED_FILE_SIZE 0
+#define MAX_ROTATION_NUM 10
+#define NO_ROTATION 0
+#define DEFAULT_ROTATE_SIZE (KB * 16)
+#define LOG_LEVEL_NOT_DECIDED (-1)
 
-static void print_usage(void) {
+static void print_usage(void)
+{
     ERR(
-    "\nUsage: aklog [-o file_name] [-a] [-l]\n"
-    "-o: output log to a file, otherwise output log to console.\n"
-    "-a: read all logs in kernel's log buffer and then exit,\n"
+    "\nUsage: aklog [-f FILE [-s kB] [-n count]] [-a] [-l]\n"
+    "-f: print log to file(s), otherwise output log to console.\n"
+    "-s: max size of a log file in kB, requires -f, cannot work\n"
+    "    with -a. When -s is not provided along with -n, size is\n"
+    "    %d kB\n"
+    "-n: max number of rotation log files up to %d, requires -f,\n"
+    "    cannot work with -a. log files are named as FILE.x.\n"
+    "-a: print all log data in kernel's log buffer and then exit,\n"
     "    otherwise aklog will keep running until it is killed.\n"
-    "-l: set log level in the range 1-8\n"
+    "-l: set log level\n",
+    DEFAULT_ROTATE_SIZE, MAX_ROTATION_NUM
     );
 }
 
@@ -53,26 +68,77 @@ static FILE *init_log_file(const char *name)
     return f;
 }
 
-static void deinit_log_file(FILE *f) {
+static void deinit_log_file(FILE *f)
+{
     if (f)
         fclose(f);
 }
 
-int main(int argc, char **argv) {
+static int do_rotation(const char *file_name, int rotate_num, FILE **pfile)
+{
+    int i;
+    char *name = NULL;
+    char *new_name = NULL;
+    int ret = 0;
+
+    if (!file_name || *file_name == '\0' || !pfile)
+        return -EINVAL;
+
+    deinit_log_file(*pfile);
+
+    for (i = rotate_num; i > 0; i--) {
+
+        ret = asprintf(&new_name, "%s.%d", file_name, i);
+
+        if (ret != -1) {
+
+            if (i == 1)
+                ret = asprintf(&name, "%s", file_name);
+            else
+                ret = asprintf(&name, "%s.%d", file_name, i - 1);
+
+            if (ret == -1) {
+                free(name);
+
+                return -ENOMEM;
+            }
+            rename(name, new_name);     // file could not exist, do it blindly
+
+            free(name);
+            free(new_name);
+        }
+        else
+            return -ENOMEM;
+    }
+
+    *pfile = init_log_file(file_name);
+
+    if (*pfile == NULL)
+        ret = -EFAULT;
+
+    return ret;
+}
+
+int main(int argc, char **argv)
+{
     int ret;
     char *buf;
-    int len;
+    int len;            // length of kernel buffer
+    int written = 0;    // how many bytes written into current log file
+    int count = 0;
     int opt;
     char * file_name = NULL;
     int read_all_once = 0;
-    int log_level = -1;
+    int log_level = LOG_LEVEL_NOT_DECIDED;
     FILE *output = NULL;
     int bytes = 0;
     int op_code;
+    long int max_file_size = UNLIMITED_FILE_SIZE;
+    int rotate_num = NO_ROTATION;
 
-    while ((opt = getopt(argc, argv, "l:o:ah")) != -1) {
+    while ((opt = getopt(argc, argv, "l:f:ahn:s:")) != -1) {
         switch (opt) {
-        case 'o':
+        case 'f':
             file_name = optarg;
             break;
         case 'a':
@@ -85,6 +151,23 @@ int main(int argc, char **argv) {
             print_usage();
 
             exit(EXIT_SUCCESS);
+        case 's':
+            max_file_size = strtol(optarg, (char **) NULL, 10);
+            if (max_file_size == LONG_MAX || max_file_size <= 0
+                    || (max_file_size *= KB) <= 0 ) {
+                print_usage();
+
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'n':
+            rotate_num = strtol(optarg, (char **) NULL, 10);
+            if (rotate_num <= NO_ROTATION || rotate_num > MAX_ROTATION_NUM) {
+                print_usage();
+
+                exit(EXIT_FAILURE);
+            }
+            break;
         default:
             print_usage();
 
@@ -101,15 +184,25 @@ int main(int argc, char **argv) {
         return -EINVAL;
     }
 
+    if ((max_file_size != UNLIMITED_FILE_SIZE || rotate_num != NO_ROTATION) &&
+            (file_name == NULL || op_code == KLOG_READ_ALL)) {
+        ERR("-s and -n only apply with '-f' and without '-a'\n");
+
+        return -EINVAL;
+    }
+
+    if (max_file_size == UNLIMITED_FILE_SIZE && rotate_num != NO_ROTATION)
+        max_file_size = DEFAULT_ROTATE_SIZE;
+
     /* set log level if user wants to change it */
-    if (log_level != -1) {
+    if (log_level != LOG_LEVEL_NOT_DECIDED) {
         ret = klogctl(KLOG_SETLEVEL, 0, log_level);
 
         if (ret) {
             ERR("failed to set log level, ret %d (%s)\n",
                     ret, strerror(errno));
 
-            return ret;
+            goto out;
         }
     }
 
@@ -119,7 +212,10 @@ int main(int argc, char **argv) {
     if (len <= 0)
         return -EFAULT;
 
-    buf = malloc(sizeof(char) * len);
+    if (max_file_size != UNLIMITED_FILE_SIZE)
+        buf = malloc(sizeof(char) * min(len, max_file_size));
+    else
+        buf = malloc(sizeof(char) * len);
 
     if (!buf) {
         ERR("failed to alloc buf mem\n");
@@ -129,15 +225,39 @@ int main(int argc, char **argv) {
     }
 
     do {
-        if ((bytes = klogctl(op_code, buf, len)) >= 0) {
+        if (max_file_size != UNLIMITED_FILE_SIZE) {
+            count = min(len, max_file_size - written);
+            if (count <= 0) {
+                if (rotate_num == NO_ROTATION)
+                    break;
+                else {
+                    ret = do_rotation(file_name, rotate_num, &output);
+                    if (ret < 0) {
+                        ERR("error when do rotation\n, stop\n");
+
+                        goto out;
+                    }
+                    else {
+                        written = 0;
+                        count = min(len, max_file_size - written);
+                    }
+                }
+            }
+        }
+        else
+            count = len;
+
+        if ((bytes = klogctl(op_code, buf, count)) >= 0) {
             if ((ret = fwrite(buf, 1, bytes, output)) != bytes) {
                 ERR("only %d of %d bytes written, stop\n", ret, bytes);
                 ret = -EFAULT;
+
                 goto out;
             }
         } else {
             ERR("klogctl returned error %d (%s)\n", bytes, strerror(errno));
             ret = bytes;
+
             goto out;
         }
 
@@ -145,6 +265,8 @@ int main(int argc, char **argv) {
 
         if (file_name)
             fsync(fileno(output));
+
+        written += bytes;
 
     } while (op_code == KLOG_READ);
 
